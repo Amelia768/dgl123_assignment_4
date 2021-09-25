1e705fbeba2cdbb6526c8a0da5f27206e1d06542
Amelia768 <71101378+Amelia768@users.noreply.github.com>
2021-09-25 15:04:08 -0700

    Add document structure

---
+++ calculator.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- 
+    DGL123
+    Week 4 Assignment
+    Amelia Manky
+    09/28/21
+    calculator.php
+-->
+<head>
+    <meta charset="UTF-8">
+    <title>Calculator</title>
+</head>
+<body>
+    
+</body>
+</html>
