c29724a16c8750313be7b552a650b44b047c3bbb
Amelia768 <71101378+Amelia768@users.noreply.github.com>
2021-09-27 15:51:03 -0700

    Add html form

--- calculator.php
+++ calculator.php
@@ -12,6 +12,17 @@
     <title>Calculator</title>
 </head>
 <body>
-    
+    <h1>Addition Only Calculator</h1>
+
+<!-- Create a form that can accept two numbers -->
+    <form>
+        <label for="first">First Number</label>
+        <input type="number" name="first" id="first" required><br>
+
+        <label for="second">Second Number</label>
+        <input type="number" name="second" id="second" required><br>
+
+        <button type="submit">Calculate Sum</button>
+    </form>
 </body>
 </html>
