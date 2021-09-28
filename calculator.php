878a7ef82df9e75bca77c52ceae060bd18916b61
Amelia768 <71101378+Amelia768@users.noreply.github.com>
2021-09-28 10:52:56 -0700

    Add documentation

--- calculator.php
+++ calculator.php
@@ -14,17 +14,21 @@
 <body>
     <h1>Addition Only Calculator</h1>
 
-<!-- Create a form that can accept two numbers -->
+<!-- Form that can accept 2 numbers -->
     <form action='' method="post">
         <label for="first">First Number</label>
-        <input type="number" name="first" id="first" required><br>
+        <input type="number" name="first" id="first" required><br> 
 
         <label for="second">Second Number</label>
         <input type="number" name="second" id="second" required><br>
 
         <button type="submit">Calculate Sum</button>
     </form>
-    <?php 
+
+<?php 
+
+    // Retrieve input values 'first' and 'second' from $_POST 
+    // Assign to variables $first and $second
     if(isset($_POST['first'], $_POST['second'])) {
         $first = $_POST['first'];
         $second = $_POST['second'];
@@ -33,8 +37,10 @@
     // Calculate the sum of the two numbers
     $sum = $first + $second;
 
+    // Display the calculation results
     print "<h3>Result:</h3>
     <p>$first + $second = $sum</p>";
-    ?>
+    
+?>
 </body>
 </html>
